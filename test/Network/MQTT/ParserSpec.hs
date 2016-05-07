dfab4fb9d286e9b8a287742c8ce0dd88a2ceda7d
drets <dmitryrets@gmail.com>
2016-05-07 14:12:00 +0200

    Check ClientId in Connect, fail on invalid UTF-8

--- test/Network/MQTT/ParserSpec.hs
+++ test/Network/MQTT/ParserSpec.hs
@@ -5,13 +5,19 @@ import Test.Hspec
 import Network.MQTT.Packet
 import Network.MQTT.Parser
 
-import Control.Monad
-import Data.Maybe
 import qualified Data.ByteString as BS
 import qualified Data.Text as T
-import Data.Attoparsec.ByteString
+import qualified Data.Vector.Mutable as VM
+import qualified Data.Vector as V
+
+import Data.Attoparsec.ByteString hiding (take)
+import Data.Word (Word8)
+import Control.Monad.ST (runST)
+import System.Random (randomRs, mkStdGen)
 
 import Data.Bits
+import Control.Monad
+import Data.Maybe
 
 isParseFail :: Result a -> Bool
 isParseFail (Fail _ _ _) = True
@@ -19,6 +25,18 @@ isParseFail _            = False
 
 shouldParseFully :: (Show a, Eq a) => Result a -> a -> Expectation
 shouldParseFully r x = r `shouldSatisfy` fromJust . compareResults (Done BS.empty x)
+
+invalidUtf8 :: [Word8]
+invalidUtf8 = [0xc0, 0xc1] ++ [0xf5 .. 0xff] ++ [0x80 .. 0xbf]
+
+shuffle :: String -> String
+shuffle xs = runST $ do
+  let vector = V.fromList xs
+  let upperBound = V.length vector - 1
+  mvector <- V.unsafeThaw vector
+  forM_ (zip [0 .. upperBound] (randomRs (0, upperBound) (mkStdGen 42))) $ uncurry (VM.swap mvector)
+  shuffled <- V.unsafeFreeze mvector
+  return $ V.toList shuffled
 
 spec :: Spec
 spec = do
@@ -48,7 +66,7 @@ spec = do
          -- clean    = 1
          -- reserved = 0
          0xce,
-         -- keep alie = 0x000c
+         -- keep alive = 0x000c
          0x00, 0x0c,
          -- client id = "abc"
          0x00, 0x03, 0x61, 0x62, 0x63,
@@ -93,7 +111,7 @@ spec = do
          -- clean    = 1
          -- reserved = 0
          0xce,
-         -- keep alie = 0x000c
+         -- keep alive = 0x000c
          0x00, 0x0c,
          -- client id = "abc"
          0x00, 0x03, 0x61, 0x62, 0x63,
@@ -149,10 +167,59 @@ spec = do
         shouldFailParsing [0x10, 0x0a, 0x00, 0x04, 0x4d, 0x51, 0x54, 0x54, 0x04, 0x40]
 
       it "MQTT-3.1.3-5: The Server MUST allow ClientIds which are between 1 and 23 UTF-8 encoded bytes in length, and that contain only the characters \"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\"" $ do
-        -- The Server MAY allow ClientId’s that contain more than 23
-        -- encoded bytes. The Server MAY allow ClientId’s that contain
-        -- characters not included in the list given above.
-        pending
+        let testString =
+              shuffle "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        forM_ [ take 1 testString
+              , take 2 testString
+              , take 23 testString
+              , testString
+              , "0123456789"
+              , "abcdefghijklmnopqrstuvwxyz"
+              , "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+              -- The Server MAY allow ClientId’s that contain more than 23
+              -- encoded bytes. The Server MAY allow ClientId’s that contain
+              -- characters not included in the list given above.
+              , "!@#$%^&*()-+"
+              ] $ \clientId ->
+          ([0x10,
+           -- remaining length
+           30 + fromIntegral (length clientId),
+           -- protocol name = MQTT
+           0x00, 0x04, 0x4d, 0x51, 0x54, 0x54,
+           -- protocol level = 4
+           0x04,
+           -- username = 1
+           -- password = 1
+           -- retain   = 0
+           -- willqos  = 01
+           -- will     = 1
+           -- clean    = 1
+           -- reserved = 0
+           0xce,
+           -- keep alive = 0x000c
+           0x00, 0x0c,
+           -- client id
+           0x00, fromIntegral (length clientId)]
+           ++
+           (fromIntegral . fromEnum <$> clientId)
+           ++
+           -- topic name = "a/b"
+           [0x00, 0x03, 0x61, 0x2f, 0x62,
+           -- will message = [0x12, 0xab]
+           0x00, 0x02, 0x12, 0xab,
+           -- username = "ac"
+           0x00, 0x02, 0x61, 0x63,
+           -- password = [0xcd, 0xbb, 0x11]
+           0x00, 0x03, 0xcd, 0xbb, 0x11]) `shouldParseAs`
+          CONNECT ConnectPacket{ connectClientIdentifier = ClientIdentifier (T.pack clientId)
+                               , connectProtocolLevel = 0x04
+                               , connectWillMsg = Just $
+                                   Message QoS1 False (Topic $ T.pack "a/b") (BS.pack [0x12, 0xab])
+                               , connectUserName = Just (UserName $ T.pack "ac")
+                               , connectPassword = Just (Password $ BS.pack [0xcd, 0xbb, 0x11])
+                               , connectCleanSession = True
+                               , connectKeepAlive = 0x000c
+                               }
 
       it "MQTT-3.1.3-6: A Server MAY allow a Client to supply a ClientId that has a length of zero bytes, however if it does so the Server MUST treat this as a special case and assign a unique ClientId to that Client" $ do
         [0x10,
@@ -170,7 +237,7 @@ spec = do
          -- clean    = 1
          -- reserved = 0
          0xce,
-         -- keep alie = 0x000c
+         -- keep alive = 0x000c
          0x00, 0x0c,
          -- client id = ""
          0x00, 0x00,
@@ -209,7 +276,7 @@ spec = do
          -- clean    = 0
          -- reserved = 0
          0xcc,
-         -- keep alie = 0x000c
+         -- keep alive = 0x000c
          0x00, 0x0c,
          -- client id = ""
          0x00, 0x00,
@@ -232,10 +299,60 @@ spec = do
                                }
 
       it "MQTT-3.1.3-10: The Will Topic MUST be a UTF-8 encoded string as defined in Section 1.5.3" $ do
-        pending
+        forM_ invalidUtf8 $ \willTopic ->
+          shouldFailParsing
+            [ 0x10
+              -- remaining length
+            , 28
+              -- protocol name = MQTT
+            , 0x00, 0x04, 0x4d, 0x51, 0x54, 0x54
+              -- protocol level = 0xf1
+            , 0x04
+              -- username = 1
+              -- password = 1
+              -- retain   = 0
+              -- willqos  = 01
+              -- will     = 1
+              -- clean    = 0
+              -- reserved = 0
+            , 0xcc
+              -- keep alive = 0x000c
+            , 0x00, 0x0c
+              -- client id = ""
+            , 0x00, 0x00
+              -- invalid topic name
+            , 0x00, 0x01, willTopic
+            ]
 
       it "MQTT-3.1.3-11: The User Name MUST be a UTF-8 encoded string as defined in Section 1.5.3" $ do
-        pending
+        forM_ invalidUtf8 $ \userName ->
+          shouldFailParsing
+            [ 0x10
+              -- remaining length
+            , 29
+              -- protocol name = MQTT
+            , 0x00, 0x04, 0x4d, 0x51, 0x54, 0x54
+              -- protocol level = 0xf1
+            , 0x04
+              -- username = 1
+              -- password = 1
+              -- retain   = 0
+              -- willqos  = 01
+              -- will     = 1
+              -- clean    = 0
+              -- reserved = 0
+            , 0xcc
+              -- keep alive = 0x000c
+            , 0x00, 0x0c
+              -- client id = ""
+            , 0x00, 0x00
+              -- topic name = "a/b"
+            , 0x00, 0x03, 0x61, 0x2f, 0x62
+              -- will message = [0x12, 0xab]
+            , 0x00, 0x02, 0x12, 0xab
+              -- invalid user name
+            , 0x00, 0x01, userName
+            ]
 
     describe "CONNACK" $ do
       it "parses valid packet" $ do
@@ -413,7 +530,8 @@ spec = do
           shouldFailParsing [0x82, len]
 
       it "MQTT-3.8.3-1: The Topic Filters in a SUBSCRIBE packet payload MUST be UTF-8 encoded strings as defined in Section 1.5.3" $ do
-        pending
+        forM_ invalidUtf8 $ \topicFilter ->
+          shouldFailParsing [0x82, 0x06, 0x12, 0xab, 0x00, 0x01, topicFilter]
 
       it "MQTT-3.8.3-4: The Server MUST treat a SUBSCRIBE packet as malformed and close the Network Connection if any of Reserved bits in the payload are non-zero, or QoS is not 0,1 or 2" $ do
         forM_ [0x03 .. 0xff] $ \flags ->
@@ -485,7 +603,8 @@ spec = do
           shouldFailParsing [0xa2, len]
 
       it "fails on non-valid utf-8" $ do
-        pending
+        forM_ invalidUtf8 $ \topicFilter ->
+          shouldFailParsing [0xa2, 0x04, 0x12, 0xab, 0x00, 0x01, topicFilter]
 
     describe "UNSUBACK" $ do
       it "parses valid packet" $ do
