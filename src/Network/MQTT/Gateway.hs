5f3d74271dae6184558763a951d33a430dbda51f
Alexey Shmalko <rasen.dubi@gmail.com>
2016-05-12 15:42:36 +0300

    Proper exception handling

--- src/Network/MQTT/Gateway.hs
+++ src/Network/MQTT/Gateway.hs
@@ -5,34 +5,46 @@ module Network.MQTT.Gateway
 
 import Network.MQTT.Parser (parsePacket)
 
-import Control.Concurrent (forkIO)
-import Control.Exception (bracket, finally)
-import Control.Monad (forever, forM_)
+import Control.Concurrent (forkIOWithUnmask)
+import Control.Exception (allowInterrupt, bracket, finally, mask_)
+import Control.Monad (forever)
 import Data.Attoparsec.ByteString (parse)
 import Network.Attoparsec (parseMany)
-import Network.Socket (AddrInfo(AddrInfo), AddrInfoFlag(AI_PASSIVE), Family(AF_INET), SockAddr(SockAddrInet), Socket, SocketType(Stream), accept, addrAddress, addrCanonName, addrFamily, addrFlags, addrProtocol, addrSocketType, bind, close, defaultProtocol, listen, socket, SocketOption(ReusePort), setSocketOption)
+import Network.Socket (AddrInfo(AddrInfo), Family(AF_INET), SockAddr(SockAddrInet), Socket, SocketType(Stream), accept, addrAddress, addrCanonName, addrFamily, addrFlags, addrProtocol, addrSocketType, bind, close, defaultProtocol, listen, socket, SocketOption(ReusePort), setSocketOption, ShutdownCmd(ShutdownBoth), shutdown)
 
 runServer :: AddrInfo -> IO ()
 runServer addr = do
-  bracket (socket (addrFamily addr) Stream defaultProtocol) close $ \sock -> do
+  bracket (socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)) close $ \sock -> do
     setSocketOption sock ReusePort 1
     bind sock (addrAddress addr)
     listen sock 5
-    forever $ forkIO . handleClient =<< accept sock
+
+    -- We want to handle all incoming socket in exception-safe way, so
+    -- no sockets leak.
+    --
+    -- Disable interrupts first to ensure no async exception is throw
+    -- between accept and setting finalizer.
+    mask_ $ forever $ do
+      -- Allow an interrupt between connections, so the thread is
+      -- still interruptible.
+      allowInterrupt
+
+      (s, a) <- accept sock
+      -- New thread is run in masked state.
+      forkIOWithUnmask $ \unmask ->
+        unmask (handleClient s a) `finally` (shutdown s ShutdownBoth >> close s)
 
 defaultMQTTAddr :: AddrInfo
-defaultMQTTAddr = AddrInfo{ addrFlags = [AI_PASSIVE]
+defaultMQTTAddr = AddrInfo{ addrFlags = []
                           , addrFamily = AF_INET
                           , addrSocketType = Stream
-                          , addrProtocol = 6
+                          , addrProtocol = defaultProtocol
                           , addrAddress = SockAddrInet (fromInteger 1883) 0
                           , addrCanonName = Nothing
                           }
 
-handleClient :: (Socket, SockAddr) -> IO ()
-handleClient (sock, addr) = do
+-- Don't need to close socket, as it's done in 'runServer' in a safer way..
+handleClient :: Socket -> SockAddr -> IO ()
+handleClient sock addr = do
   putStrLn $ "Connected: " ++ show addr
-  flip finally (close sock) $ do
-    (_, res) <- parseMany sock (parse parsePacket) (parse parsePacket)
-    forM_ res print
-  close sock
+  print . snd =<< parseMany sock (parse parsePacket) (parse parsePacket)
