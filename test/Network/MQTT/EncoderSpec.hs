29feb88f23858df3cef1dc141d54f1eb0b6df5da
drets <dmitryrets@gmail.com>
2016-05-15 01:32:11 +0200

    Enhance program coverage for Encoder

--- test/Network/MQTT/EncoderSpec.hs
+++ test/Network/MQTT/EncoderSpec.hs
@@ -3,19 +3,19 @@
 module Network.MQTT.EncoderSpec (spec) where
 
 import           Network.MQTT.Packet
-import           Network.MQTT.Encoder             (encodePacket)
-import           Network.MQTT.Parser              (parsePacket)
+import           Network.MQTT.Encoder             ( encodePacket )
+import           Network.MQTT.Parser              ( parsePacket )
 
-import           Test.SmallCheck.Series           (Serial)
-import           Test.SmallCheck                  ((==>))
+import           Test.SmallCheck.Series           ( (<~>), Serial, decDepth , getNonEmpty, series )
+import           Test.SmallCheck                  ( (==>) )
 import           Test.Hspec
-import           Test.Hspec.SmallCheck            (property)
+import           Test.Hspec.SmallCheck            ( property )
 import           Test.SmallCheck.Series.Instances ()
-import           Data.Attoparsec.ByteString       (parseOnly)
-import           Data.ByteString.Builder          (toLazyByteString)
-import           Data.ByteString.Lazy             (toStrict)
-import qualified Data.Text as T
-import           Data.Maybe                       (isNothing, isJust)
+import           Data.Attoparsec.ByteString       ( parseOnly )
+import           Data.ByteString.Builder          ( toLazyByteString )
+import           Data.ByteString.Lazy             ( toStrict )
+import qualified Data.Text                        as T
+import           Data.Maybe                       ( isJust, isNothing )
 
 spec :: Spec
 spec = do
@@ -26,32 +26,34 @@ spec = do
 
 valid :: Packet -> Bool
 valid packet = case packet of
-  SUBSCRIBE SubscribePacket{..}     ->
-    not (null subscribeTopicFiltersQoS) &&
-    all (not . T.null . unTopicFilter . fst) subscribeTopicFiltersQoS
-  SUBACK SubackPacket{..}           -> not $ null subackResponses
-  UNSUBSCRIBE UnsubscribePacket{..} ->
-    not (null unsubscribeTopicFilters) &&
-    all (not . T.null. unTopicFilter) unsubscribeTopicFilters
   CONNACK ConnackPacket{..}         -> not (connackReturnCode /= Accepted && connackSessionPresent)
   PUBLISH PublishPacket{..}         ->
     not ((messageQoS publishMessage /= QoS0 && isNothing publishPacketIdentifier) ||
-         (messageQoS publishMessage == QoS0 && (publishDup || isJust publishPacketIdentifier)) ||
-         (T.null . unTopic $ messageTopic publishMessage))
+         (messageQoS publishMessage == QoS0 && (publishDup || isJust publishPacketIdentifier)))
   CONNECT ConnectPacket{..}         -> not (isNothing connectUserName && isJust connectPassword)
   _                                 -> True
 
 instance Monad m => Serial m Packet
-instance Monad m => Serial m ConnectPacket
+instance Monad m => Serial m ConnectPacket where
+    series = ConnectPacket <$> decDepth (decDepth series)
+                           <~> decDepth (decDepth (decDepth series))
+                           <~> series
+                           <~> decDepth series
+                           <~> decDepth series
+                           <~> decDepth (decDepth (decDepth series))
+                           <~> decDepth (decDepth (decDepth series))
 instance Monad m => Serial m ConnackPacket
 instance Monad m => Serial m PublishPacket
 instance Monad m => Serial m PubackPacket
 instance Monad m => Serial m PubrecPacket
 instance Monad m => Serial m PubrelPacket
 instance Monad m => Serial m PubcompPacket
-instance Monad m => Serial m SubscribePacket
-instance Monad m => Serial m SubackPacket
-instance Monad m => Serial m UnsubscribePacket
+instance Monad m => Serial m SubscribePacket where
+  series = SubscribePacket <$> series <~> (getNonEmpty <$> series)
+instance Monad m => Serial m SubackPacket where
+  series = SubackPacket <$> series <~> (getNonEmpty <$> series)
+instance Monad m => Serial m UnsubscribePacket where
+  series = UnsubscribePacket <$> series <~> (getNonEmpty <$> series)
 instance Monad m => Serial m UnsubackPacket
 instance Monad m => Serial m PingreqPacket
 instance Monad m => Serial m PingrespPacket
@@ -64,5 +66,7 @@ instance Monad m => Serial m ClientIdentifier
 instance Monad m => Serial m PacketIdentifier
 instance Monad m => Serial m UserName
 instance Monad m => Serial m Password
-instance Monad m => Serial m Topic
-instance Monad m => Serial m TopicFilter
+instance Monad m => Serial m Topic where
+  series = Topic <$> (T.pack . getNonEmpty) <$> series
+instance Monad m => Serial m TopicFilter where
+  series = TopicFilter <$> (T.pack . getNonEmpty) <$> series
