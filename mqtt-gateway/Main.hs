ae5e2fd9026b1e3dd78c402dcfd7cfe90543f4ad
Dmytro Rets <dmitryrets@gmail.com>
2016-08-07 22:18:22 +0200

    Use URI

--- mqtt-gateway/Main.hs
+++ mqtt-gateway/Main.hs
@@ -1,11 +1,13 @@
 module Main (main) where
 
-import           Data.IP                      (IPv4, toHostAddress)
 import           Network                      (withSocketsDo)
 import           Network.MQTT.Client          as Client
 import qualified Network.MQTT.Gateway         as Gateway
 import           Network.Socket               (SockAddr (SockAddrInet), Socket,
                                                addrAddress)
+import           Network.Socket               (getAddrInfo)
+import           Network.URI                  (URI, parseURI, uriAuthority,
+                                               uriPort, uriRegName)
 import           System.Environment           (getArgs)
 import           System.IO.Streams            (InputStream, OutputStream)
 import qualified System.IO.Streams.Concurrent as S
@@ -14,23 +16,31 @@ import           System.Log.Logger            (Priority (DEBUG), rootLoggerName,
 
 main :: IO ()
 main = do
+  let err = error "Must supply a valid MQTT broker URI"
   args <- getArgs
   case args of
-    []   -> error "Must supply a MQTT broker IP address"
+    [uri] -> do
+      case parseURI uri of
+        Just u -> do
+          case uriAuthority u of
+            Just authority -> do
+              updateGlobalLogger rootLoggerName (setLevel DEBUG)
+              withSocketsDo $ do
+                client <- startClient (uriRegName authority) (uriPort authority)
+                gw     <- Gateway.newGateway client
+                Gateway.listenOnAddr gw Gateway.defaultMQTTAddr
+            Nothing -> err
+        Nothing -> err
+    _    -> err
 
-    [ip] -> do
-      updateGlobalLogger rootLoggerName (setLevel DEBUG)
-      withSocketsDo $ do
-        client <- startClient (read ip)
-        gw     <- Gateway.newGateway client
-        Gateway.listenOnAddr gw Gateway.defaultMQTTAddr
-
-    _    -> error "Too many arguments"
-
-startClient :: IPv4 -> IO (InputStream ClientResult, OutputStream ClientCommand)
-startClient ip = do
+startClient :: String -> String -> IO (InputStream ClientResult, OutputStream ClientCommand)
+startClient regName port = do
   (client_result, client_result') <- S.makeChanPipe
-  let brokerAddr = Gateway.defaultMQTTAddr { addrAddress = SockAddrInet 1883 (toHostAddress ip) }
+  brokerAddr:_ <- getAddrInfo Nothing (Just regName) (getServiceName port)
   -- TODO how to gracefully close Client's socket?
   (socket, client_command) <- Client.runClientWithSockets Client.defaultClientConfig client_result' brokerAddr
   return (client_result, client_command)
+    where
+      getServiceName []    = Nothing
+      getServiceName [':'] = Nothing
+      getServiceName p     = Just p
